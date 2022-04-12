ac29cf4f9206f99e70cf7e951e1474cd8658908d
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-12 15:12:48 +0000

    spinner1

--- spinner1.js
+++ spinner1.js
@@ -4,71 +4,20 @@ setTimeout (() => {
 
 setTimeout (() => {
     process.stdout.write('\r/ ');
-},300);
+},600);
 
 setTimeout (() => {
     process.stdout.write('\r- ');
-},500);
-
-setTimeout (() => {
-    Process.stdout.write('\r\ ');
-},700);
-
-
-setTimeout (() => {
-    process.stdout.write('\r| ');
-},1000);
-
-setTimeout (() => {
-    process.stdout.write('\r/  ');
-},1300);
-
-setTimeout (() => {
-    process.stdout.write('\r-  ');
-},1500);
-
-setTimeout (() => {
-    Process.stdout.write('\r/  ');
-},1700);
-
-setTimeout (() => {
-    Process.stdout.write('\r-  ');
-},2000);
-
-
-setTimeout (() => {
-    process.stdout.write('\r| ');
-},2300);
+},1100);
 
 setTimeout (() => {
     process.stdout.write('\r/ ');
-},2600);
+},1600);
 
 setTimeout (() => {
     process.stdout.write('\r- ');
-},2900);
+},2200);
 
 setTimeout (() => {
-    Process.stdout.write('\r\ ');
-},3200);
-
-
-setTimeout (() => {
-    process.stdout.write('\r| ');
-},3600);
-
-setTimeout (() => {
-    process.stdout.write('\r/  ');
-},4000);
-
-setTimeout (() => {
-    process.stdout.write('\r-  ');
-},4300);
-
-setTimeout (() => {
-    Process.stdout.write('\r/  ');
-}, 4600);
-
-setTimeout (() => {
-    Process.stdout.write('\r-\  ');
-},5000);
+    process.stdout.write('\r|\ ');
+},2600);
